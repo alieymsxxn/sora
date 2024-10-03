f953b80324e464d2b027cdaaa854be222079c7e4
Mohsin Ali <mohsinalisep@gmail.com>
2024-10-04 03:00:04 +0500

    Integrated staticfiles and updated code

--- src/sora/settings.py
+++ src/sora/settings.py
@@ -25,11 +25,12 @@ SECRET_KEY = 'django-insecure-_l8gisd5&-s@7vdcxinthlwd!u&n9%0!3i10$c+cnqc8b)-so=
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config(option='DEBUG', cast=bool, default=True)
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] if DEBUG else ['.railway.app']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app'] if DEBUG else ['.railway.app']
+print('DEBUG:', DEBUG)
 # Application definition
 STARTED_APPS = [
     'visits',
+    'console',
 ]
 
 INSTALLED_APPS = [
@@ -84,7 +85,7 @@ DATABASES = {
 }
 
 DB_CONN_MAX_AGE = config(option='CONN_MAX_AGE', cast=int, default=30)
-DB_CONN_STRR = config(option='DB_CONN_STRR', cast=str, default=None)
+DB_CONN_STRR = config(option='DB_CONN_STRR', default=None)
 
 if DB_CONN_STRR:
     import dj_database_url
@@ -131,6 +132,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
+
+# Source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+# Output of python manage.py collectstatic
+# Local CDN
+STATIC_ROOT = BASE_DIR / 'local-cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
