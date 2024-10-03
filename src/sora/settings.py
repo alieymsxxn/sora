c73acb553826e047649434c57a7e194238a0bfa7
Mohsin Ali <mohsinalisep@gmail.com>
2024-10-04 03:50:01 +0500

    Fixed missing ARGs bug

--- src/sora/settings.py
+++ src/sora/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
-
+print('SECRET_KEY', SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config(option='DEBUG', cast=bool, default=True)
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app'] if DEBUG else ['.railway.app']
