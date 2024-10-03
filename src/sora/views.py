9a37e845bf4d794a4a8d7228eaf0bbac693ee426
Mohsin Ali <mohsinalisep@gmail.com>
2024-10-04 03:07:07 +0500

    Integrated flowbite nav and created a view

--- src/sora/views.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-from django.http import HttpResponse
-from django.shortcuts import render
-   
-def home_page(request, *args, **kwargs):
-    context = {
-        'title': 'Big Title'
-    }
-    return render(request=request, template_name='home.html', context=context)
--- src/visits/urls.py
+++ src/visits/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, reverse, reverse_lazy
+from visits.views import homepage
 
 urlpatterns = [
-    path('', include('visits.urls'), name='visits'), # index page / root page
-    path('admin/', admin.site.urls),
+    path('', homepage, name='lala')
 ]
