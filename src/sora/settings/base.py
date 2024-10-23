c6b30275a9f0cac2fff9d1d36f3842fb8e78f591
Mohsin Ali <mohsinalisep@gmail.com>
2024-10-23 22:10:12 +0500

    Removed S3 storage cus I am broke

--- src/sora/settings/base.py
+++ src/sora/settings/base.py
@@ -187,3 +187,21 @@ STORAGES = {
         'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage', 
     },
 }
+
+# collectstatic will accumulate staticfiles here
+STATIC_ROOT = BASE_DIR / 'local-cdn'
+# URL used to refer staticfiles inside STATIC_ROOT
+STATIC_URL = 'static/'
+# Generated path for collectstatic to look for staticfiles
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_BASE_DIR.mkdir(parents=True, exist_ok=True)
+# Defining source(s) for python manage.py collectstatic to collect from
+STATICFILES_DIRS = [ STATICFILES_BASE_DIR ]
+# Generated path for vendor, custom staticfiles for custom usage
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / 'vendors'
+STATICFILES_CUSTOM_DIR = STATICFILES_BASE_DIR / 'assets'
+# Backends that define where to find staticfiles from
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
